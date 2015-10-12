88f84e643921c581784d50f6416d27091e914f79
OneEyedBanshee <craig_johnsto@msn.com>
2015-10-12 15:03:32 +0100

    spine merged manually

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,18 @@
 #pragma comment(lib,"opengl32.lib") 
 #pragma comment(lib,"glu32.lib") 
 
+#include "spine/spine-sfml.h"
 #include "SFML/Graphics.hpp" 
+#include <SFML/Window/Mouse.hpp>
 #include "SFML/OpenGL.hpp" 
 #include <iostream> 
+#include <string.h>
 #define _USE_MATH_DEFINES
 #include <math.h>
+
+using namespace std;
+using namespace spine;
+#include <stdio.h>
 
 #pragma comment(lib, "zlibwapi.lib")
 
@@ -36,11 +43,75 @@
 
 #include "WaterShaderText.h"
 
+/*void callback(AnimationState* state, int trackIndex, EventType type, Event* event, int loopCount) {
+	TrackEntry* entry = AnimationState_getCurrent(state, trackIndex);
+	const char* animationName = (entry && entry->animation) ? entry->animation->name : 0;
+
+	switch (type) {
+	case ANIMATION_START:
+		printf("%d start: %s\n", trackIndex, animationName);
+		break;
+	case ANIMATION_END:
+		printf("%d end: %s\n", trackIndex, animationName);
+		break;
+	case ANIMATION_COMPLETE:
+		printf("%d complete: %s, %d\n", trackIndex, animationName, loopCount);
+		break;
+	case ANIMATION_EVENT:
+		printf("%d event: %s, %s: %d, %f, %s\n", trackIndex, animationName, event->data->name, event->intValue, event->floatValue,
+			event->stringValue);
+		break;
+	}
+	fflush(stdout);
+}*/
+
 int main()
 {
+
+#pragma region spineSetup
+	// Load atlas, skeleton, and animations.
+	Atlas* atlas = Atlas_createFromFile("data/spineboy.atlas", 0);
+	SkeletonJson* json = SkeletonJson_create(atlas);
+	json->scale = 0.3f;
+	SkeletonData *skeletonData = SkeletonJson_readSkeletonDataFile(json, "data/spineboy.json");
+	if (!skeletonData) {
+		printf("%s\n", json->error);
+		exit(0);
+	}
+	SkeletonJson_dispose(json);
+	SkeletonBounds* bounds = SkeletonBounds_create();
+
+	// Configure mixing.
+	AnimationStateData* stateData = AnimationStateData_create(skeletonData);
+	AnimationStateData_setMixByName(stateData, "walk", "jump", 0.2f);
+	AnimationStateData_setMixByName(stateData, "jump", "run", 0.2f);
+
+	SkeletonDrawable* drawable = new SkeletonDrawable(skeletonData, stateData);
+	drawable->timeScale = 1;
+
+	Skeleton* skeleton = drawable->skeleton;
+	skeleton->flipX = false;
+	skeleton->flipY = false;
+	Skeleton_setToSetupPose(skeleton);
+
+	skeleton->x = 200;
+	skeleton->y = 460;
+	Skeleton_updateWorldTransform(skeleton);
+
+	Slot* headSlot = Skeleton_findSlot(skeleton, "head");
+
+	//drawable->state->listener = callback;
+	if (false) {
+		AnimationState_setAnimationByName(drawable->state, 0, "test", true);
+	}
+	else {
+		AnimationState_setAnimationByName(drawable->state, 0, "walk", true);
+		AnimationState_addAnimationByName(drawable->state, 0, "jump", false, 3);
+		AnimationState_addAnimationByName(drawable->state, 0, "run", true, 0);
+	}
+#pragma endregion spineSetup
+
 	bool shader = false, showDebug = false;
-
-
 	tmx::MapLoader ml("resources");
 	if (shader)	ml.Load("shader_example.tmx"); else	ml.Load("isometric_grass_and_water.tmx");
 	ml.UpdateQuadTree(sf::FloatRect(0.f, 0.f, 800.f, 600.f));
@@ -53,7 +124,7 @@ int main()
 	sf::Shader waterEffect;
 	waterEffect.loadFromMemory(waterShader, sf::Shader::Fragment);
 	if (shader)	ml.SetLayerShader(0u, waterEffect);
-	sf::Clock shaderClock, frameClock;
+	sf::Clock shaderClock, frameClock, deltaClock;
 
 	//load a font
 	sf::Font font;
@@ -121,6 +192,26 @@ int main()
 		//update shader
 		if (shader) waterEffect.setParameter("time", shaderClock.getElapsedTime().asSeconds());
 
+#pragma region drawingSpine
+
+		float delta = deltaClock.getElapsedTime().asSeconds();
+		deltaClock.restart();
+
+		SkeletonBounds_update(bounds, skeleton, true);
+		sf::Vector2i position = sf::Mouse::getPosition(window);
+		if (SkeletonBounds_containsPoint(bounds, position.x, position.y)) {
+			headSlot->g = 0;
+			headSlot->b = 0;
+		}
+		else {
+			headSlot->g = 1;
+			headSlot->b = 1;
+		}
+
+		drawable->update(delta);
+
+#pragma endregion drawingSpine
+
 
 		//prepare frame
 		window.clear();
@@ -130,11 +221,16 @@ int main()
 		if (showDebug) ml.Draw(window, tmx::MapLayer::Debug);//draw with debug info
 		window.draw(screenPosText);
 		window.draw(mapPosText);
+		window.draw(*drawable);
 		// Finally, display rendered frame on screen 
 		window.display();
 
 		window.setTitle("3rd Year Project " + std::to_string(1.f / frameClock.getElapsedTime().asSeconds()));
 	} //loop back for next frame
+
+	SkeletonData_dispose(skeletonData);
+	SkeletonBounds_dispose(bounds);
+	Atlas_dispose(atlas);
 
 	return EXIT_SUCCESS;
 }
